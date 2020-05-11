e9ee8d7c595402a4986c2b3d8270f616555a2332
amal-san <amaldemaniax@gmail.com>
2020-05-11 10:34:21 +0530

    game logic and data mapping

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,8 +11,20 @@ const Game = () => {
 
 	const winner = calculateWinner(board);
 
-    const handleClick = () => {
-        
+    const handleClick = i => {
+        const boardCopy = [...board];
+
+        //If user click an occupied square or if game is won, return
+
+        if(winner || boardCopy[i]) return;
+
+        // Put an X or an O in the clicked square
+
+        boardCopy[i] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setXisNext(!xIsNext);
+
+
     }
     
     const jumpTo = () => {
