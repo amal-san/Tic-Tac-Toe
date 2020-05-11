5bdf7a542d386811f43032be34e5b0abd0dbb018
amal-san <amaldemaniax@gmail.com>
2020-05-11 10:13:55 +0530

    styled square components

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-
+const style = {
+	background:'lightblue',
+	border:'2px solid darkblue',
+	fontSize:'30px',
+	fontWeight:'800',
+	cursor:'pointer',
+	outline:'none',
+	padding:'10px'
+}
 
 const Square = ({ value , onClick }) => (
-	<button onClick={onClick}>
+	<button style={style} onClick={onClick}>
 		{value}
 	</button>
 );
