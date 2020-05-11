24e4ce8aa65450f68b6989243dd9c3f6c3824ae1
amal-san <amaldemaniax@gmail.com>
2020-05-11 10:01:49 +0530

    basic board, squares components with eventhanlder

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Board from './Board'
 
-const Game = () => <div> Game </div>
+
+const Game = () => {
+
+    const handleClick = () => {
+        
+    }
+    
+    const jumpTo = () => {
+        
+    }
+    
+    const renderMoves = () => {
+        
+    }
+
+	return (
+		<Board onClick={handleClick}/>
+	);
+	
+
+}
 
 export default Game;
