5e7a0a0a4161d75a07fa464c012d99b660d12585
amal-san <amaldemaniax@gmail.com>
2020-05-11 11:17:08 +0530

    Updates

---
+++ precache-manifest.68c5e9548cfa7b99cf1af2b9249b6e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2962139c906c8ba0b5d764dbed89aec",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "376cf7cc6d2daa6dbc70",
+    "url": "/Tic-Tac-Toe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "faa5dbee3df738b994e0",
+    "url": "/Tic-Tac-Toe/static/js/2.54615f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.54615f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376cf7cc6d2daa6dbc70",
+    "url": "/Tic-Tac-Toe/static/js/main.68efeed4.chunk.js"
+  },
+  {
+    "revision": "5892d02b6ec4af5a9e86",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.64d9acbf.js"
+  }
+]);
